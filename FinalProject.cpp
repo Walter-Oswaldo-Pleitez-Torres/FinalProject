6aa498bdee12f9d073933a3448b642392b3a885d
Walter-Oswaldo-Pleitez-Torres <00143023@uca.edu.sv>
2023-06-30 02:40:16 -0600

    Todos los calculos funcionan bien pero TotalOrden da problema cuando es 1 en todo

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -19,10 +19,24 @@ struct Fecha
         "Diciembre",
     };
 };
+struct Inventario
+{
+    string Productos[8] = {"SANDWICH", "PUPUSAS", "CROISANDWICH", "PAPAS", "CAFE", "CHOCOLATE", "SODA", "LICUADO"};
+    float Precios[8] = {0.75, 0.35, 0.50, 1.00, 0.35, 0.40, 0.50, 1.25};
+};
 
+//DECLARACION DE FUNCIONES
 string NombreMes(struct Fecha, int PosicionesMes); //BUSCADOR DE NOMBRE DE MES
 bool InicioSesion(string Usuario, string Clave);
+void InicioSesionAdmin(string Usuario, string Clave);
 void Ordenar(int, string, int); //RECIBE FECHA Y SE COMIENZA A LLEVAR LAS ORDENES
+void MenuDesplegable();
+int BuscadorPosicion(string NombreItem, struct Inventario);  //BUSCA POSICION EN NOMBRES PRODUCTOS (STRING)
+float BuscadorPrecio(int, struct Inventario);                //CON LA POSICION DE PRODUCTOS BUSCA EL PRECIO (FLOAT)
+float PagoIndividual(float, int);                            //CALCULA EL PEDIDO DE UN CLIENTE DE LA ORDEN (QUE PUEDE CONTENER MAS)
+float SumaMontoPedidos(float PagoGral[], int CantProductos); //SUMA LOS MONTOS DE PEDIDOS DE UN CLIENTE
+void Factura(int, string[], float[], float[]);               // IMPRIME LOS DATOS DE LOS PEDIDOS DE UN CLIENTE
+float MontoTotalOrden(float[], int);                         // CALCULA EL TOTAL DE LA ORDEN
 
 int main()
 {
@@ -53,7 +67,7 @@ int main()
                 string Usuario, Clave;
                 int Opcion;
 
-                cout << "1) SISTEMA DE VENTAS  2) HISTORIAL" << endl;
+                cout << "1) SISTEMA DE VENTAS  2) ADMINISTRACION" << endl;
                 cin >> Opcion;
 
                 do //MENU PRINCIPAL
@@ -61,7 +75,7 @@ int main()
 
                     switch (Opcion)
                     {
-                    case 0://ROMPER EL TIEMPO SIMULADO
+                    case 0: //ROMPER EL TIEMPO SIMULADO
                         cout << endl;
                         cout << "OPCION SECRETA DE FORMATEO" << endl;
                         cout << "Seguira rompiendo el tiempo, Ingrese 0 " << endl;
@@ -91,7 +105,7 @@ int main()
                         cin >> Usuario;
                         cout << "Ingrese la clave de Administrador: " << endl;
                         cin >> Clave;
-                        //InicioSesionAdmin(Usuario, Clave);
+                        InicioSesionAdmin(Usuario, Clave); //LEER FICHEROS, ETC..
 
                         break;
                     }
@@ -132,6 +146,8 @@ int main()
 
     return 0;
 }
+
+//DEFINICION DE FUNCIONES
 
 string NombreMes(struct Fecha, int PosicionMes)
 {
@@ -180,7 +196,9 @@ string NombreMes(struct Fecha, int PosicionMes)
 
     return MesActual;
 }
+
 bool InicioSesion(string Usuario, string Clave)
+
 {
     string User1 = "user1", Clave1 = "12345";
     string User2 = "user2", Clave2 = "54321";
@@ -203,25 +221,195 @@ bool InicioSesion(string Usuario, string Clave)
         return false;
     }
 }
+
+void InicioSesionAdmin(string Usuario, string Clave)
+{
+    string User1 = "Admin1", Clave1 = "12345";
+    string User2 = "Admin2", Clave2 = "54321";
+    bool Acceso1 = (Usuario == User1) && (Clave == Clave1);
+    bool Acceso2 = (Usuario == User2) && (Clave == Clave2);
+
+    if ((Acceso1) || (Acceso2))
+    {
+        cout << "ACCESO CONCEDIDO" << endl;
+        cout << endl;
+        //LeerFichero();
+    }
+    else
+    {
+        cout << endl;
+        cout << "ACCESO DENEGADO" << endl;
+    }
+}
+
 void Ordenar(int Dia, string Mes, int Year)
 { //RECEPCION DE ORDENES
+    struct Inventario Menu;
     int ContinuarOrden = 1;
-    int CantidadPersonas, ContadorOrden = 0;
+    int CantPersonas=0, CantProductos, CantItem;
+    int ContOrdenes = 0, ContPedidos = 0, ContProductos = 0;
+    int ItemPosicion;
+    float ItemPrecio, ItemPago, SumaItemPago, PagoOrden = 0;
+    string ItemNombre;
     cout << endl;
-    cout << "Fecha: " << Dia << " / " << Mes << " / " << Year << endl;
 
     do
-    {
+    { //BUCLE DE ORDENES: Una orden puede tener Pedidos de varias personas o solo una
+        cout << "Fecha: " << Dia << " / " << Mes << " / " << Year << endl;
         cout << "*********** AREA DE RECEPCION DE ORDENES ***************" << endl;
+        ContOrdenes++; //SERVIRA EN LA ESCRITURA DEL FILE
         cout << "Para cuantas personas desean ordenar: " << endl;
-        cin >> CantidadPersonas;
-        ContadorOrden++;
+        cin >> CantPersonas; //SERVIRA PARA CALCULAR LOS PAGOS TOTALES DE LA ORDEN
+
+        float TotalOrden[CantPersonas]; //SUMA LOS PAGOS DE CADA PERSONA EN UNA ORDEN L 309
+
+        for (int i = 0; i < CantPersonas; i++) //INGRESO DE PEDIDOS: Cada Cliente de la orden hace Pedido
+        {
+            ContPedidos++; //SERVIRA EN LA ESCRITURA DEL FILE
+            MenuDesplegable();
+            cout << "*************** Cliente: " << i + 1 << "***************" << endl;
+            cout << "Cuantos Productos del Menu llaman su atencion: " << endl;
+            cin >> CantProductos;
+
+            string CarritoNombre[CantProductos]; //ALMACENARA LOS NOMBRES DE PRODUCTOS PEDIDOS
+            float CarritoPrecio[CantProductos];  //ALMACENARA LOS PRECIOS DE PRODUCTOS PEDIDOS
+            float CarritoPago[CantProductos];    //ALMACENARA LOS PAGOS POR PERSONA DE PRODUCTOS PEDIDOS
+
+            for (int j = 0; j < CantProductos; j++) //RECIBE TODOS LOS PEDIDOS DE UNA PERSONA
+            {
+                cout << endl;
+                cout << "*************** Ingrese Pedido ***************" << endl;
+                ContProductos++; //SERVIRA EN LA ESCRITURA DEL FILE
+                cout << "Ingrese el nombre del producto que desea comprar: " << endl;
+                cin >> ItemNombre;
+                BuscadorPosicion(ItemNombre, Menu);                //BUSCA NOMBRE DE PRODUCTO INGRESADO Y SU POSICION
+                ItemPosicion = BuscadorPosicion(ItemNombre, Menu); //NOMBRE Y PRECIO TIENEN MISMA POSICION
+                ItemPrecio = BuscadorPrecio(ItemPosicion, Menu);   //ENCUENTRA Y ALAMACENA EL PRECIO DEL PRODUCTO
+
+                cout << "Ingrese la cantidad de " << ItemNombre << endl;
+                cin >> CantItem; //PIDE LA CANTIDAD DE UN MISMO PRODUCTO DESEADO
+
+                /*LLAMAR A FUNCION CREAR FICHERO (AQUI)*/
+
+                cout << "*************** FIN Ingreso de Pedido ***************" << endl;
+
+                PagoIndividual(ItemPrecio, CantItem); //CALCULA EL PAGO DE UN PEDIDO DE UN CLIENTE
+                ItemPago = PagoIndividual(ItemPrecio, CantItem);
+
+                CarritoNombre[j] = ItemNombre; //ALMACENA EL NOMBRE DEL PRODUCTO DESEADO
+                CarritoPrecio[j] = ItemPrecio; //ALMACENA EL PRECIO A PAGAR POR UN MISMO PRODUCTO
+                CarritoPago[j] = ItemPago;     //ALMACENA EL MONTO DEL PRODUCTO INGRESADO
+            }                                  //FIN DE PEDIDOS DE UNA PERSONA DE LA ORDEN (QUE PUEDE TENER MAS DE UNA)
+
+            SumaItemPago = SumaMontoPedidos(CarritoPago, CantProductos);
+
+            Factura(CantProductos, CarritoNombre, CarritoPrecio, CarritoPago); //IMPRIME EL DETALLE DE COMPRA POR CLIENTE
+
+            cout << "Total a Pagar por Persona: " << SumaItemPago << endl;
+            TotalOrden[i] = SumaItemPago; //ALMACENA EL TOTAL DE LOS PEDIDOS DE UN CLIENTE
+
+        } //FIN BUCLE INGRESO DE PEDIDOS E IMPRESIONDE FACTURA PARA UNA PERSONA
+
+        PagoOrden = MontoTotalOrden(TotalOrden, CantPersonas);
+
+        cout << " " << endl;
+        cout << "*********************************************************" << endl;
+        cout << "El Total a Pagar para todas las personas de la Orden es de: $" << PagoOrden << endl;
+        cout << "*********************************************************" << endl;
+        cout << " " << endl;
 
         cout << endl;
         cout << "PERMANECER recibiendo las Ordenes de: " << Dia << "/" << Mes << " Ingrese 1, si desea SALIR Ingrese 0: " << endl;
         cout << endl;
         cin >> ContinuarOrden;
-    } while (ContinuarOrden == 1);
-
-    float TotalOrden[CantidadPersonas];
+    } while (ContinuarOrden == 1); //FIN BUCLE ORDENES
 }
+
+int BuscadorPosicion(string NombreItem, struct Inventario NombresProductos)
+{
+    for (int i = 0; i < NombreItem.length(); i++) //CONVIERTE TODAS LAS PALABRAS EN MAYUSCULAS
+    {
+        NombreItem[i] = towupper(NombreItem[i]);
+    }
+
+    int Posicion;
+    for (int i = 0; i < 8; i++) //ENCUENTRA LA POSICION DEL PRODUCTO EN EL ARRAY
+    {
+        if (NombreItem == NombresProductos.Productos[i])
+        {
+            Posicion = i;
+        }
+    }
+    return Posicion;
+}
+
+float BuscadorPrecio(int PosicionPrecio, struct Inventario Precio)
+{ //Aqui entra la posicion del precio que es la misma del producto
+    float PrecioProducto;
+    PrecioProducto = Precio.Precios[PosicionPrecio];
+
+    return PrecioProducto;
+}
+
+float PagoIndividual(float PrecioItem, int CantItem) //CALCULA EL PAGO POR EL PEDIDO DE UN CLIENTES
+{
+    float Pagar = PrecioItem * CantItem;
+    return Pagar;
+}
+
+float SumaMontoPedidos(float ItemPagos[], int CantProductos) //CALCULO EL PAGO DE UNA PERSONA POR SUS PEDIDOS
+{
+    float TotalGral;
+    for (int i = 0; i < CantProductos; i++)
+    {
+        TotalGral = TotalGral + ItemPagos[i];
+    }
+    return TotalGral;
+}
+
+void Factura(int CantProductos, string CarritoNombre[], float CarritoPrecio[], float CarritoPago[])
+{
+    for (int k = 0; k < CantProductos; k++) //IMPRIME TODOS LOS PEDIDOS DE UNA PERSONA
+    {
+        cout << endl;
+        cout << "--------- Vea su Pedido ----------" << endl;
+        cout << "Nombre: " << CarritoNombre[k] << endl;
+        cout << "Precio: " << CarritoPrecio[k] << endl;
+        cout << "Total por Pedido: " << CarritoPago[k] << endl;
+        cout << "---------- Fin Pedido ----------" << endl;
+    } //FIN BUCLE DE IMPRESION DE PEDIDOS EN CONSOLA
+}
+
+float MontoTotalOrden(float Totales[], int CantClientes)
+{
+    float SumaTotalPagar;
+    for (int i = 0; i < CantClientes; i++)
+    {
+        SumaTotalPagar = SumaTotalPagar + Totales[i];
+    }
+    cout<<"PRUEBA DE RESULTADO DE FUNCION: "<<SumaTotalPagar<<endl;
+
+    return SumaTotalPagar;
+}
+
+void MenuDesplegable()
+{
+    cout << " " << endl;
+    cout << " " << endl;
+    cout << "************ MENU ************" << endl;
+    cout << "------------ COMIDA RAPIDA ------------" << endl;
+    cout << "1) Sandwich $0.75" << endl;
+    cout << "2) Pupusas $0.35 c/u" << endl;
+    cout << "3) Croisandwich $0.50" << endl;
+    cout << "4) Papas" << endl;
+
+    cout << "------------ BEBIDAS CALIENTES ------------" << endl;
+    cout << "5) Cafe $0.35" << endl;
+    cout << "6) Chocolate $0.40" << endl;
+
+    cout << "------------ BEBIDAS FRIAS ------------" << endl;
+    cout << "7) Soda $0.50" << endl;
+    cout << "8) Licuado $1.25" << endl;
+    cout << "************************" << endl;
+    cout << " " << endl;
+} //FIN MENU
